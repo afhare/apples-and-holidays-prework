0302d4b8814cdfbc28342a67fee9c3ebd3fad7ba
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-18 00:36:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,6 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supply|
       puts " #{holiday.to_s.split(" ").map {|x| x.capitalize}.join(' ')}: #{supply.join(", ")}"
     end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
