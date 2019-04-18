c583455b7d0a88625a77f69215226519053fb8a2
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-18 00:11:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holidays|
    holidays.each do |holiday, supply|
-      if supply.include("BBQ")
+      if supply.include?("BBQ")
       bbq_holiday << holiday
       end
     end
